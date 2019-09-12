3732155d49041cd686ee4544ca8f16410e123971
dmi3coder <dmi3coder@gmail.com>
2019-09-12 10:11:44 +0300

    Removed example because of uslessness of javafx functionality, switching to android and web

--- src/test/java/de/dmi3y/behaiv/LibraryTest.java
+++ src/test/java/de/dmi3y/behaiv/LibraryTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.*;
 
 public class LibraryTest {
     @Test public void testSomeLibraryMethod() {
-        Behaiv classUnderTest = new Behaiv();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+//        Behaiv classUnderTest = new Behaiv();
+//        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
 }
