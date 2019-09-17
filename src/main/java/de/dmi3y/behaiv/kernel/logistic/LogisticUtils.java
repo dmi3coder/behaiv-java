6aeced3abca8d9dcdb33703bfe4fd0cceb43aa71
dmi3coder <dmi3coder@gmail.com>
2019-09-17 18:36:06 +0300

    Added working logistic regression

--- src/main/java/de/dmi3y/behaiv/kernel/logistic/LogisticUtils.java
+++ src/main/java/de/dmi3y/behaiv/kernel/logistic/LogisticUtils.java
@@ -1,5 +1,7 @@
 package de.dmi3y.behaiv.kernel.logistic;
 
+import org.ejml.data.DMatrixRMaj;
+import org.ejml.dense.row.CommonOps_DDRM;
 import org.ejml.simple.SimpleMatrix;
 
 public final class LogisticUtils {
@@ -7,7 +9,41 @@ public final class LogisticUtils {
     }
 
     public static SimpleMatrix sigmoid(SimpleMatrix x) {
-        return x.negative().elementExp().plus(1).pseudoInverse();
+        final SimpleMatrix bottom = x.negative()
+                .elementExp()
+                .plus(1);
+        final DMatrixRMaj result = bottom.getDDRM();
+        CommonOps_DDRM.divide(1.0, result);
+        return new SimpleMatrix(result);
+    }
+
+    public static SimpleMatrix costFunction(SimpleMatrix x, SimpleMatrix theta, SimpleMatrix y) {
+        SimpleMatrix h_x = sigmoid(x.mult(theta));
+
+        final SimpleMatrix log_h = h_x.elementLog();
+        final int m = y.numCols();
+        return y.transpose().negative()
+                .mult(log_h)
+                .minus(
+                        y.negative().plus(1).transpose()
+                                .mult(
+                                        h_x.negative().plus(1).elementLog() // (1 - y)T
+                                )
+                )
+                .divide(m);
+    }
+
+    public static SimpleMatrix gradient(SimpleMatrix x, SimpleMatrix theta, SimpleMatrix y) {
+        final int m = y.numCols();
+        final SimpleMatrix h_x = sigmoid(x.mult(theta));
+        final SimpleMatrix loss = h_x.minus(y);
+        final SimpleMatrix gradient = x.transpose().mult(loss).divide(m);
+        return gradient;
+
+    }
+
+    public static SimpleMatrix gradientDescent(SimpleMatrix x, SimpleMatrix theta, SimpleMatrix y, double alpha) {
+        return theta.minus(gradient(x, theta, y).scale(alpha));
     }
 
 
--- src/test/java/de/dmi3y/behaiv/kernel/logistic/LogisticUtilsTest.java
+++ src/test/java/de/dmi3y/behaiv/kernel/logistic/LogisticUtilsTest.java
@@ -1,16 +1,97 @@
 package de.dmi3y.behaiv.kernel.logistic;
 
+import de.dmi3y.behaiv.kernel.KernelTest;
+import org.ejml.dense.row.CommonOps_DDRM;
 import org.ejml.simple.SimpleMatrix;
 import org.junit.Test;
 
+import java.util.Random;
+
+import static de.dmi3y.behaiv.kernel.KernelTest.HOME;
+import static de.dmi3y.behaiv.kernel.KernelTest.JOG;
+import static de.dmi3y.behaiv.kernel.KernelTest.WORK;
 import static junit.framework.TestCase.assertEquals;
 
 public class LogisticUtilsTest {
 
+    public static SimpleMatrix SINGLE_VALUE_MATRIX = new SimpleMatrix(new double[][]{new double[]{10.0}});
+    public static SimpleMatrix MULTIPLE_VALUE_MATRIX = new SimpleMatrix(new double[][]{new double[]{10.0, 1.0, 4.0, 6.0, 9.0, 2.0}}).transpose();
 
     @Test
     public void sigmoid_feed10_receive0_99995() {
         final SimpleMatrix zigmoid = LogisticUtils.sigmoid(new SimpleMatrix(new double[][]{new double[]{10.0}}));
         assertEquals(0.9999546021312976, zigmoid.get(0, 0));
+    }
+
+
+    @Test
+    public void verify_1minusM_equalsTo_negativeMPlus1() {
+        final SimpleMatrix plus = SINGLE_VALUE_MATRIX.negative().plus(1);
+        assertEquals(-9.0, plus.get(0, 0));
+    }
+
+
+    @Test
+    public void costFunction_feedMatrix_receiveCorretResult() {
+        final SimpleMatrix x = SINGLE_VALUE_MATRIX;
+        final SimpleMatrix theta = new SimpleMatrix(new double[][]{new double[]{0.01}});
+        final SimpleMatrix y = new SimpleMatrix(new double[][]{new double[]{1.0}});
+        final SimpleMatrix cost = LogisticUtils.costFunction(x, theta, y);
+        System.out.println(cost.toString());
+    }
+
+    @Test
+    public void gradientDescent_feedMatrix_receiveCorrectResult() {
+
+        for (int i = 0; i < 100; i++) {
+            SimpleMatrix x = new SimpleMatrix(KernelTest.getTrainingArray()); //14x3
+            final SimpleMatrix y = new SimpleMatrix(KernelTest.getLabelsArray());//14x4
+            Random rand = new Random();
+            SimpleMatrix theta = SimpleMatrix.random_DDRM(3, 4, 0, 1, rand); //3x4?
+
+            for (int j = 0; j < 10000; j++) {
+                theta = LogisticUtils.gradientDescent(x, theta, y, 0.1);
+            }
+
+            x = new SimpleMatrix(new double[][]{
+                    new double[]{
+                            (16 * 60 + 30.0) / (24 * 60),
+                            WORK[0],
+                            WORK[1]}
+            });
+            theta.print("%f");
+            SimpleMatrix result = LogisticUtils.sigmoid(x.mult(theta));
+            double max = CommonOps_DDRM.elementMax(result.getDDRM());
+            assertEquals(max, result.get(0, 2));
+
+            x = new SimpleMatrix(new double[][]{
+                    new double[]{
+                            (5 * 60 + 10.0) / (24 * 60),
+                            HOME[0],
+                            HOME[1]}
+            });
+            result = LogisticUtils.sigmoid(x.mult(theta));
+//            result.print("%f");
+            max = CommonOps_DDRM.elementMax(result.getDDRM());
+
+            assertEquals(max, result.get(0, 0));
+
+
+//            theta.print("%f");
+            x = new SimpleMatrix(new double[][]{
+                    new double[]{
+                            (19 * 60 + 15.0) / (24 * 60),
+                            JOG[0],
+                            JOG[1]}
+            });
+            result = LogisticUtils.sigmoid(x.mult(theta));
+//            result.print("%f");
+            max = CommonOps_DDRM.elementMax(result.getDDRM());
+
+            assertEquals(max, result.get(0, 1));
+
+            System.out.println("\n");
+//            theta.print("%f");
+        }
     }
 }
