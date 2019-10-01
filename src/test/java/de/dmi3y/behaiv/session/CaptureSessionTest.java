8f940be019d1bbb62c9f62116e1409293efef3a6
dmi3coder <dmi3coder@gmail.com>
2019-10-01 10:53:14 +0300

    unit test refactoring

--- src/test/java/de/dmi3y/behaiv/session/CaptureSessionTest.java
+++ src/test/java/de/dmi3y/behaiv/session/CaptureSessionTest.java
@@ -39,10 +39,7 @@ public class CaptureSessionTest {
         final List<Pair<Double, String>> features = session.getFeatures();
         for (int i = 0; i < 5; i++) {
             assertEquals(features.get(i).getKey(), i + 1.0);
-        }
-        for (int i = 0; i < 5; i++) {
             assertEquals(features.get(i).getValue(), "ord" + (i + 1));
         }
-
     }
 }
