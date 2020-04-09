d850ec288faec9c84b81ce8eef0a38409eedc495
dmi3coder <dmi3coder@gmail.com>
2020-04-09 11:50:48 +0300

    Small refactoring of DecisionTreeUtils

--- src/main/java/de/dmi3y/behaiv/kernel/decision_tree/DecisionTreeUtils.java
+++ src/main/java/de/dmi3y/behaiv/kernel/decision_tree/DecisionTreeUtils.java
@@ -17,39 +17,20 @@ public final class DecisionTreeUtils {
     private static final String KEY_LEFT = "left";
     private static final String KEY_RIGHT = "right";
 
-
-    private static double[][] dataset = {{2.771244718, 1.784783929, 0.0},
-            {1.728571309, 1.169761413, 0.0},
-            {3.678319846, 2.81281357, 0.0},
-            {3.961043357, 2.61995032, 0.0},
-            {2.999208922, 2.209014212, 0.0},
-            {7.497545867, 3.162953546, 1.0},
-            {9.00220326, 3.339047188, 1.0},
-            {7.444542326, 0.476683375, 1.0},
-            {10.12493903, 3.234550982, 1.0},
-            {6.642287351, 3.319983761, 1.0}};
-
     private DecisionTreeUtils() {
-    }
-
-    public static void main(String[] args) {
-        final SimpleMatrix dataSetmatrix = new SimpleMatrix(dataset);
-        final HashMap<String, Object> stringObjectHashMap1 = buildTree(dataSetmatrix, 3, 1);
-        final Double prediction = predict(stringObjectHashMap1, new SimpleMatrix(new double[][]{{10.12493903, 3.234550982, 1.0}}));
-        System.out.println(prediction);
     }
 
     public static Double predict(HashMap<String, Object> node, SimpleMatrix row) {
         final Double index = (Double) node.get("index");
         final Double value = (Double) node.get("value");
-        if(row.get(index.intValue()) < value){
-            if(node.get(KEY_LEFT) instanceof HashMap) {
+        if (row.get(index.intValue()) < value) {
+            if (node.get(KEY_LEFT) instanceof HashMap) {
                 return predict((HashMap<String, Object>) node.get(KEY_LEFT), row);
             } else {
                 return (Double) node.get(KEY_LEFT);
             }
         } else {
-            if(node.get(KEY_RIGHT) instanceof HashMap) {
+            if (node.get(KEY_RIGHT) instanceof HashMap) {
                 return predict((HashMap<String, Object>) node.get(KEY_RIGHT), row);
             } else {
                 return (Double) node.get(KEY_RIGHT);
@@ -69,7 +50,7 @@ public final class DecisionTreeUtils {
         final ArrayList<SimpleMatrix> left = group.getKey();
         final ArrayList<SimpleMatrix> right = group.getValue();
         node.remove(KEY_GROUPS);
-        if(left.isEmpty() || right.isEmpty()) {
+        if (left.isEmpty() || right.isEmpty()) {
             final ArrayList<SimpleMatrix> both = new ArrayList<>();
             both.addAll(left);
             both.addAll(right);
@@ -78,39 +59,29 @@ public final class DecisionTreeUtils {
             return;
         }
 
-        if(depth >= max_depth) {
+        if (depth >= max_depth) {
             node.put(KEY_LEFT, toTerminalNode(left));
             node.put(KEY_RIGHT, toTerminalNode(right));
             return;
         }
-        if(left.size() <= min_size) {
-            node.put(KEY_LEFT, toTerminalNode(left));
-        } else {
-            double[][] leftSet = new double[left.size()][left.get(0).numCols()];
+        splitSide(node, max_depth, min_size, depth, left, KEY_LEFT);
 
-            for (int i = 0; i < left.size(); i++) {
-                leftSet[i] = ((DMatrixRMaj) left.get(i).getMatrix()).data;
+        splitSide(node, max_depth, min_size, depth, right, KEY_RIGHT);
+    }
+
+    private static void splitSide(HashMap<String, Object> node, int max_depth, int min_size,
+                                  int depth, ArrayList<SimpleMatrix> side, String sideKey) {
+        if (side.size() <= min_size) {
+            node.put(sideKey, toTerminalNode(side));
+        } else {
+            double[][] leftSet = new double[side.size()][side.get(0).numCols()];
+
+            for (int i = 0; i < side.size(); i++) {
+                leftSet[i] = ((DMatrixRMaj) side.get(i).getMatrix()).data;
             }
 
-            node.put(KEY_LEFT, splitDataset(new SimpleMatrix(leftSet)));
-            split((HashMap<String, Object>) node.get(KEY_LEFT), max_depth, min_size, depth + 1);
-        }
-
-        if(right.size() <= min_size) {
-            node.put(KEY_RIGHT, toTerminalNode(right));
-        } else {
-//            final SimpleMatrix simpleMatrix = new SimpleMatrix(right.size(), right.get(0).numCols());
-//            ((DMatrixRMaj) simpleMatrix.getMatrix());
-//            final DMatrixRMaj dMatrixRMaj = new DMatrixRMaj();
-//
-            double[][] rightSet = new double[right.size()][right.get(0).numCols()];
-
-            for (int i = 0; i < right.size(); i++) {
-                rightSet[i] = ((DMatrixRMaj) right.get(i).getMatrix()).data;
-            }
-
-            node.put(KEY_RIGHT, splitDataset(new SimpleMatrix(rightSet)));
-            split((HashMap<String, Object>) node.get(KEY_RIGHT), max_depth, min_size, depth + 1);
+            node.put(sideKey, splitDataset(new SimpleMatrix(leftSet)));
+            split((HashMap<String, Object>) node.get(sideKey), max_depth, min_size, depth + 1);
         }
     }
 
@@ -143,7 +114,7 @@ public final class DecisionTreeUtils {
                 final SimpleMatrix row = dataset.rows(rowi, rowi + 1);
                 final Groups groups = testSplit(index, row.get(index), dataset);
                 final double gini = giniIndex(groups, classes);
-                if(gini < bScore) {
+                if (gini < bScore) {
                     bIndex = index;
                     bValue = row.get(index);
                     bScore = gini;
@@ -173,11 +144,10 @@ public final class DecisionTreeUtils {
             }
             double score = 0.0;
             for (Double classValue : classes) {
-                final ArrayList<Double> groupY = new ArrayList<>();
                 int classValueCount = 0;
                 for (SimpleMatrix value : group) {
                     final double v = value.get(value.numCols() - 1);
-                    if(v == classValue) {
+                    if (v == classValue) {
                         classValueCount += 1;
                     }
                 }
@@ -206,10 +176,6 @@ public final class DecisionTreeUtils {
     public static class Groups extends Pair<ArrayList<SimpleMatrix>, ArrayList<SimpleMatrix>> implements Serializable {
         public Groups(ArrayList<SimpleMatrix> simpleMatrices, ArrayList<SimpleMatrix> simpleMatrices2) {
             super(simpleMatrices, simpleMatrices2);
-        }
-
-        public int[] length() {
-            return new int[]{getKey().size(), getValue().size()};
         }
 
         public int total() {
